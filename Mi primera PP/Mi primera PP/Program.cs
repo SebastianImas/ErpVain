126cbb6b196dddd0dcc47c5a090a1f4c675f49e6
Sebastian <simas@vain-arm.cl>
2016-01-19 12:30:34 -0300

    simas@vain-arm.cl

---
+++ Mi primera PP/Mi primera PP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mi_primera_PP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
